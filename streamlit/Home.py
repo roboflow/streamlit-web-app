1edff0016526c74a8fb11ecac0b7e73bb3151413
Mohamed Traore <89935638+mo-traor3-ai@users.noreply.github.com>
2022-12-16 14:34:42 -0600

    Add files for app and test image
    adding the code that runs the pages of the app, and a test image for inference

---
+++ streamlit/Home.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+
+st.write("""
+# roboflow/streamlit-object-detection
+Interface with the Roboflow API and Python package for running inference (receiving predictions) from your Roboflow Train computer vision models.
+
+![Contact Us!](https://i.imgur.com/rBmXoQ1.png)
+### [Website](https://roboflow.com/) • [Login/Signup](https://app.roboflow.com/)✅ • [Roboflow Universe](https://universe.roboflow.com)🚀 • [Docs](https://docs.roboflow.com)🗒 • 
+### [Blog](https://blog.roboflow.com)✍️ • [GitHub](https://github.com/roboflow/)💻 • [Forum](https://discuss.roboflow.com)🗣 • [Notebooks (GitHub)](https://github.com/roboflow/notebooks)📓
+### [Knowledge Base](https://help.roboflow.com)📚 • [Twitter](https://twitter.com/roboflow) • [Linkedin](https://www.linkedin.com/company/roboflow-ai)
+
+# What is Roboflow?
+### **Roboflow** makes managing, preprocessing, augmenting, and versioning datasets for computer vision seamless.
+
+### This project utilizes the official [Roboflow python package](https://docs.roboflow.com/python) that interfaces with the [Roboflow API](https://docs.roboflow.com/inference/hosted-api),
+### and the new [roboflow-computer-vision-utilities](https://github.com/roboflow-ai/roboflow-computer-vision-utilities/) repo.
+### This sample app can be a great first step to help you derive actionable insights from your deployed computer vision models.
+
+## Key features of Roboflow:
+- [Import](https://docs.roboflow.com/adding-data) and [Export](https://docs.roboflow.com/exporting-data) image datasets into any supported [formats](https://roboflow.com/formats)
+- [Preprocess](https://docs.roboflow.com/image-transformations/image-preprocessing)
+  and [augment](https://docs.roboflow.com/image-transformations/image-augmentation) data using Roboflow's dataset
+  management tools
+- Train computer vision models using [Roboflow Train](https://docs.roboflow.com/train) and deploy
+  to production
+    - [Test Deploys](https://blog.roboflow.com/deploy-tab) | [Full Deployment](https://docs.roboflow.com/inference)
+- Use [community curated projects](https://universe.roboflow.com/) to start building your own vision-powered products
+
+### [Features by workspace type](https://roboflow.com/pricing)
+
+### Personal and Research Projects - Applying for Additional Account Features and Project or Research Sponsorship:
+* https://roboflow.com/community
+
+### Business Projects and POC's - Requesting Additional Account Features:
+* https://roboflow.com/sales
+""")
---
+++ streamlit/pages/1_About.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+
+st.write("""
+# Getting Started in Computer Vision
+#### Learn the Basics: https://roboflow.com/learn
+### [Computer vision (also called machine vision)](https://blog.roboflow.com/intro-to-computer-vision/) is one of the most exciting frontiers in computing. With computer vision, anyone with the right skills can solve problems that used to take years of research to solve.
+### You can build a model to detect defects in products, to identify wildfires, or to detect the wildlife in a given area. If a solution benefits from identifying or tracking objects in video or finding objects in an image, computer vision can be helpful. https://universe.roboflow.com/browse
+
+#### How to Train a Model with Roboflow Train:
+* https://docs.roboflow.com/train
+
+#### Testing Your Model with the Deploy Tab:
+* https://blog.roboflow.com/deploy-tab
+""")
+
+st.write("# How to Use this App:")
+
+video_file = open('pages/Roboflow_Streamlit-Object-Detection.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+st.write("""
+#### Each video below provides a 60-second overview into a different computer vision problem-type.
+##### 1. Object Detection
+[![Roboflow ML1M: What is Object Detection](https://img.youtube.com/vi/FM4-Pvrvo14/0.jpg)](https://www.youtube.com/watch?v=FM4-Pvrvo14)
+
+##### 2. Image Classification
+[![Roboflow ML1M: What is Image Classification](https://img.youtube.com/vi/0swerezO3EQ/0.jpg)](https://www.youtube.com/watch?v=0swerezO3EQ)
+
+##### 3. [Instance] Segmentation
+[![Roboflow ML1M: What is Instance Segmentation](https://img.youtube.com/vi/jLwWaN_2Omo/0.jpg)](https://www.youtube.com/watch?v=jLwWaN_2Omo)
+""")
+
+st.write("""
+### Roboflow Blog:
+#### [Object Detection vs. Image Classification vs. Keypoint Detection](https://blog.roboflow.com/object-detection-vs-image-classification-vs-keypoint-detection/)
+
+#### [Semantic Segmentation vs. Instance Segmentation: Explained](https://blog.roboflow.com/difference-semantic-segmentation-instance-segmentation/)
+""")
